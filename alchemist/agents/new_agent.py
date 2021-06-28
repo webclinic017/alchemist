31f325d52e26111156efc57e32970ccc16e25c78
Richard Somovidis <richard@somovidis.com>
2021-06-28 17:41:30 +0100

    Completed transfer and testing of classifier agent

--- alchemist/agents/new_agent.py
+++ alchemist/agents/new_agent.py
@@ -1,6 +1,10 @@
+import os
+import math
+import warnings
 import torch as T
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import numpy as np
 
 class Agent(nn.Module):
@@ -14,15 +18,17 @@ class Agent(nn.Module):
         self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
 
         # Layers, but not like an oinion
-        self.conv1 = nn.Conv2d(1, 1, 1)
-        self.maxpool1 = nn.MaxPool2d(1)
-        self.conv2 = nn.Conv2d(1, 1, 1)
-        self.maxpool2 = nn.MaxPool2d(1)
+        kernel_size = 3
+        padding = math.floor(kernel_size / 2)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size, padding = padding)
+        self.maxpool1 = nn.MaxPool2d(kernel_size, padding = padding, stride = 1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size, padding = padding)
+        self.maxpool2 = nn.MaxPool2d(kernel_size, padding = padding, stride = 1)
         # Boring Layers
         self.input_dims = self.calc_input_dims(self.n_features, self.feature_length)
-        self.fc1 = nn.Linear(self.input_dims, 1)
-        self.fc2 = nn.Linear(1, 1)
-        self.fc3 = nn.Linear(1, 1)
+        self.fc1 = nn.Linear(self.input_dims, self.input_dims * 2)
+        self.fc2 = nn.Linear(self.input_dims * 2, self.input_dims)
+        self.fc3 = nn.Linear(self.input_dims, 2)
 
         # Optimizer etc.
         self.optimizer = optim.SGD(self.parameters(), lr=1e-2, momentum=0.9)
@@ -30,11 +36,17 @@ class Agent(nn.Module):
         self.to(self.device)
 
         # Data loaders
+        # print(train_ds.x_data)
         self.train_data_loader = T.utils.data.DataLoader(train_ds,
                     batch_size = batch_size, shuffle=True, num_workers=8)
+        # for x, y in enumerate(self.train_data_loader): print(x, y)
         self.test_data_loader = None if test_ds == None else (
                 T.utils.data.DataLoader(train_ds, batch_size = batch_size, 
                                         shuffle=True, num_workers=8))
+
+        # For documentation, testing etc.
+        self.loss_history = []
+        self.acc_history = []
 
     def calc_input_dims(self, n_features, feature_length):
         # NOTE: This is very necessary!!! It's the input dims of the first
@@ -46,6 +58,163 @@ class Agent(nn.Module):
         x = self.maxpool2(x)
 
         return int(np.prod(x.size()))
+
+
+    def forward(self, x):
+        # Make sure input is a tensor, and push it to gpu if possible
+        # This throws a warning that I can't fix, so we ignore it
+        x = x.to(self.device)
+        # x = x.type(T.float32)
+        # Apply convolutional layers, relu and maxpool
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.maxpool2(x)
+        # Rearrange input to be a 1-dimentional tensor for the fc layers
+        x = x.view(x.size()[0], -1)
+        # Now apply the fully-connected layers
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+    def save_chkpt(self, path):
+        # Make sure that the folders the file is meant to be in exist
+        if not os.path.exists(path[:path.rindex("/")]):
+            os.makedirs(path[:path.rindex("/")])
+        # Then save the file
+        state = {
+            "state_dict" : self.state_dict(),
+            "optimizer" : self.optimizer.state_dict()
+        }
+        T.save(state, path)
+
+    def load_chkpt(self, path = None):
+        # Load the checkpoint
+        state = T.load(path)
+        # Initialize state_dict from checkpoint to model
+        self.load_state_dict(state['state_dict'])
+        # Initialize optimizer from checkpoint to optimizer
+        self.optimizer.load_state_dict(state['optimizer'])
+
+    def _train(self, epochs, verbose = False):
+        # Introduces some random variation to aid with training
+        self.train()
+        # Clear acc and loss history for documentation
+        acc_history = []
+        loss_history = []
+
+        for ep in range(epochs):
+            ep_loss = 0
+            ep_acc = []
+            # print("-------------", self.train_data_loader.x_data)
+            for j, (input, label) in enumerate(self.train_data_loader):
+                # --- Very vital steps ---
+                self.optimizer.zero_grad()
+                # This throws a warning that I can't fix, so we ignore it
+                # with warnings.catch_warnings():
+                    # warnings.simplefilter("ignore")
+                label = label.to(self.device, dtype = T.long)
+                input = input.to(dtype = T.float32)
+                # print(type(input))
+
+                # print(input.shape)
+                # input = T.tensor(input)
+                prediction = self.forward(input)
+                # prediction = prediction.to(dtype = T.float64)
+                # prediction = T.squeeze(prediction)
+                # print(prediction, label)
+                # print(prediction.dtype, input.dtype)
+                # exit()
+                loss = self.loss(prediction, label)
+                loss.backward()
+                self.optimizer.step()
+                # ---                  ---
+                # +++ Pretty much just documentation +++
+                # print(prediction)
+                prediction = F.softmax(prediction, dim=1)
+                # print(prediction)
+                classes = T.argmax(prediction, dim=1)
+                wrong = T.where(classes != label,
+                                T.Tensor([1.]).to(self.device),
+                                T.Tensor([0.]).to(self.device))
+                acc = 1 - T.sum(wrong) / self.batch_size
+
+                ep_loss += loss.item()
+                ep_acc.append(acc.item())
+                # +++                                +++
+            acc_history.append(ep_acc)
+            loss_history.append(ep_loss)
+
+            # NOTE: "verbose" needs better implementation
+            if verbose: print("Finished epoch", ep, 
+                              " total loss % .3f" % ep_loss,
+                              " accuracy %.3f" % np.mean(ep_acc))
+
+        return acc_history, loss_history
+
+    def _test(self, epochs = 5):
+        # Makes sure there isn't any random variation, as in training
+        self.eval()
+        # Clear history for documentation
+        loss_history = []
+        acc_history = []
+
+        for i in range(epochs):
+            ep_loss = 0
+            ep_acc = []
+            for j, (input, label) in enumerate(self.test_data_loader):
+                # --- Steps not needed for testing ---
+                # self.optimizer.zero_grad()
+                # loss.backward()
+                # self.optimizer.step()
+                # ---                              ---
+                # +++ The rest of it +++
+                label = label.to(self.device, dtype = T.long)
+                input = input.to(dtype = T.float32)
+                prediction = self.forward(input)
+                loss = self.loss(prediction, label)
+                prediction = F.softmax(prediction, dim=1)
+                classes = T.argmax(prediction, dim=1)
+                wrong = T.where(classes != label,
+                                T.Tensor([1.]).to(self.device),
+                                T.Tensor([0.]).to(self.device))
+                acc = 1 - T.sum(wrong) / self.batch_size
+
+                # NOTE: Below is code for recording things that tripped up
+                #       the agent, currently inactive but supposedly functional.
+                # fails = []
+                # # Convert tensors to lists
+                # classes = classes.tolist()
+                # label = label.tolist()
+                # input = input.tolist()
+                # # Get indexes of wrong responses
+                # for k in range(len(classes)):
+                #     if classes[k] != label[k]:
+                #         # Add the correct label that was guessed wrong
+                #         fails.append(label[k])
+                # # Only print fails if there are any
+                # if len(fails) != 0:
+                #     # print(fails)
+                #     # Identify the actual lists that tripped us up
+                #     for k in fails:
+                #         bad_list = input[k]
+                #         # print(bad_list)
+                #         self.fails.append(bad_list)
+
+                ep_loss += loss.item()
+                ep_acc.append(acc.item())
+                acc_history.append(acc.item())
+            loss_history.append(ep_loss)
+                # +++                +++
+
+            # print("Finished epoch", i, " total loss % .3f" % ep_loss,
+                    # "accuracy %.3f" % np.mean(ep_acc))
+        return acc_history, loss_history
+
 
 
 
--- tests/agents/test_new_agent.py
+++ tests/agents/test_new_agent.py
@@ -1,5 +1,7 @@
+import os
 import unittest
 import torch as T
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from alchemist.data.ticker_data import *
@@ -8,6 +10,9 @@ from alchemist.agents.new_agent import *
 # The new agent should have the brain of the old one, with improvements
 # on how it interfaces with the rest of the code; these tests define this,
 # and should be appliccable to any agent buit to replace the new agent
+
+# NOTE: Most of this is probably the wrong way to test a PyTorch CNN. However I
+# haven't been able to find the "right" way, and it feels wrong to leave it untested
 
 class TestAgentCreation(unittest.TestCase):
 
@@ -54,28 +59,54 @@ class TestAgentCreation(unittest.TestCase):
 class TestAgentFunctionality(unittest.TestCase):
 
     def setUp(self):
-        data = get_data(path = "cache/tests/TestAgentFunctionality", tickers = ["GME", "TSLA"], 
-                        from_date = "2020-01-01", to_date = "2021-01-01")
-        data = format_into_percentages(data, formatting_basis = "daily open")
-        x, y = format_into_xy(data, num_features = 5)
-        # In some cases we don't need to test the agent, and provide a single ds
-        self.dataset = TickerDataset(x, y)
-        # In other cases we provide both a train and a test dataset
-        self.train_ds, self.test_ds = train_test_datasets(x, y)
+        # Using really obvious, but still 2d data just to test everything works as it should
+        x = [[[3.0,3.0,3.0], [3.0,3.0,3.0]]] * 50
+        y = [1] * 50
+        x += [[[-1.0,-1.0,-1.0], [-1.0,-1.0,-1.0]]] * 50
+        y += [0] * 50
+        train_ds, test_ds = train_test_datasets(x, y)
+        self.agent = Agent(train_ds, test_ds, batch_size = 8)
+
+    def test_padding(self):
+        x = T.zeros((1, 1, 3, 2))
+        x = self.agent.conv1(x)
+        x = self.agent.maxpool1(x)
+        x = self.agent.conv2(x)
+        x = self.agent.maxpool2(x)
+        self.assertEqual(x.size(), T.Size([1, 32, 3, 2]))
 
     def test_forward(self):
-        pass
+        x = T.zeros((1, 1, 3, 2))
+        y = self.agent.forward(x)
+        self.assertEqual(y.size(), T.Size([1, 2]))
+
+    def test_save_load_agent(self):
+        # I'm not quite sure how to test state dicts etc, this shouls be fine
+        path = "cache/tests/agents/test_load_save_agent"
+        if os.path.isfile(path):
+            os.remove(path)
+        self.agent.save_chkpt(path = path)
+        # Check the save file has been made, try loading from it
+        self.assertTrue(os.path.isfile(path))
+        self.agent.load_chkpt(path = path)
 
     def test_train(self):
-        pass
+        # Run the train function
+        acc_h, loss_h = self.agent._train(epochs = 5)
+        # The acc and loss history should show improvement
+        self.assertTrue(len(acc_h) > 0)
+        self.assertTrue(len(loss_h) > 0)
+        self.assertEqual(np.mean(acc_h[-1]), 1)
+        self.assertAlmostEqual(np.mean(loss_h[-1]), 0, 2)
 
     def test_test(self):
-        pass
-    
-    def test_save_agent(self):
-        pass
-
-    def test_load_agent(self):
-        pass
+        # Test an untrained agent
+        acc_hu, loss_hu = self.agent._test()
+        # Test a trained agent
+        self.agent._train(epochs = 5)
+        acc_ht, loss_ht = self.agent._test()
+        # Compare
+        self.assertTrue(np.mean(acc_ht) > np.mean(acc_hu))
+        self.assertTrue(np.mean(loss_ht) < np.mean(loss_hu))
 
 
