e59e1f38901cfa0daaa2be9ae985ac234c52fab9
professional-username <richard@somovidis.com>
2021-07-12 13:00:09 +0100

    Added backtests to long tests

--- tests/long/test_classifying_ticker_data.py
+++ tests/long/test_classifying_ticker_data.py
@@ -1,4 +1,6 @@
+import math
 import unittest
+import torch as T
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -141,5 +143,125 @@ class TestWithChangingHyperparameters(unittest.TestCase):
         path = "cache/tests/plots/ticker_agent_testing/hyperparameter_tuning/volatility_adjusted_lower_lr"
         graph_train_data(data = train_hist, path = path, step = 5)
 
+class TestActualGains(unittest.TestCase):
+    
+    def setUp(self):
+        self.path = "cache/tests/data/TestNewAgentForActualGains"
+        self.raw_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                        from_date = "2010-01-01", to_date = "2021-01-01")
+        # self.data = format_into_percentages(self.raw_data, formatting_basis = "daily close")
+        # x, y = format_into_xy(self.data, num_features = 5, label_type = "bin", 
+                # label_type_vars = {"divider" : 1, "balance" : True})
+        # train_ds, test_ds = train_test_datasets(x, y)
 
+        # self.agent = Agent(train_ds, test_ds)
 
+    def backtest(self, agent, x, y, n_tickers = 1):
+        # x data should be formatted for the agent, y data only into percentages
+        # print(x)
+        x = T.tensor(x, dtype = T.float32)
+        x = T.unsqueeze(x, 1)
+        prediction = agent.forward(x)
+        # print(prediction)
+        prediction = F.softmax(prediction, dim=1)
+        # print(prediction)
+        choices = T.argmax(prediction, dim=1)
+        choices = list(choices)
+        
+        choices = [list(j) for j in np.array_split(choices, n_tickers)]
+        choices = list(map(list, zip(*choices)))
+        y = [list(j) for j in np.array_split(y, n_tickers)]
+        y = list(map(list, zip(*y)))
+        # gains = [(y[i] if choices[i] == 1 else 1) for i in range(len(y[0]))]
+        # gains = [((np.sum([y[j][i] if choices[j][i] == 1 else 1]) - [choices[j][i]].count(1))
+            # /(n_tickers-[choices[j][i]].count(1)) for j in range(n_tickers)) for i in range(len(y[0]))]
+        gains = [(np.sum([y[i][j] if choices[i][j] == 1 else 1 for j in range(n_tickers)]) 
+            - choices[i].count(0))/(n_tickers-choices[i].count(0)) for i in range(len(y))]
+        gains = [i if not math.isnan(i) else 1 for i in gains]
+        # print(y[:5])
+        # print(choices[:5])
+        # print(gains[:5])
+
+        total = np.product(gains)
+        average = np.mean(gains)
+        print("Total:", total, "or %.3f" % (total*100) + "%")
+        print("Average:", average, "or %.3f" % ((average-1)*100) + "%")
+        print("Days:", len(y))
+        # print(choices.count(0))
+        # print(gains.count(1))
+        # print(gains)
+        path = "cache/tests/plots/ticker_agent_testing/backtests/backtest.png"
+        graph_backtest_data(gains, path)
+
+    def test_basic_backtest(self):
+        # As seen several times above, but without a train-test split
+        training_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                                 from_date = "2010-01-01", to_date = "2018-01-01")
+        training_data = format_into_percentages(training_data, formatting_basis = "daily open")
+        train_x, train_y = format_into_xy(training_data, num_features = 5, label_type = "bin",
+                label_type_vars = {"divider" : 1, "balance" : True})
+        train_ds = TickerDataset(train_x, train_y)
+        agent = Agent(train_ds)
+        agent._train(epochs = 1000, verbose = True)
+        print("Training done!")
+
+        # The test data is simple to format, thanks to 'label_type = "float"'
+        test_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                             from_date = "2018-01-01", to_date = "2021-01-01")
+        test_data = format_into_percentages(test_data, formatting_basis = "daily open")
+        test_x, test_y = format_into_xy(test_data, num_features = 5, label_type = "float",
+                label_type_vars = {"divider" : 1, "balance" : False})
+
+        print("Beginning backtest...")
+        # backtest does all the rest of the work
+        self.backtest(agent, test_x, test_y, len(test_data["Data"].keys()))
+
+    def test_volatility_backtest(self):
+        # As seen several times above, but without a train-test split
+        training_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                                 from_date = "2010-01-01", to_date = "2018-01-01")
+        training_data = format_into_percentages(training_data, formatting_basis = "daily open")
+        training_data = adjust_for_volatility(training_data, volatility_type = "daily v")
+        train_x, train_y = format_into_xy(training_data, num_features = 5, label_type = "bin",
+                label_type_vars = {"divider" : 1, "balance" : True})
+        train_ds = TickerDataset(train_x, train_y)
+        agent = Agent(train_ds, learning_rate = 5e-3)
+        agent._train(epochs = 1000, verbose = True)
+        print("Training done!")
+
+        # The test data is simple to format, thanks to 'label_type = "float"'
+        test_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                             from_date = "2018-01-01", to_date = "2021-01-01")
+        test_data = format_into_percentages(test_data, formatting_basis = "daily open")
+        not_x, test_y = format_into_xy(test_data, num_features = 5, label_type = "float",
+                label_type_vars = {"divider" : 1, "balance" : False})
+        test_data = adjust_for_volatility(test_data, volatility_type = "daily v")
+        test_x, not_y = format_into_xy(test_data, num_features = 5, label_type = "float",
+                label_type_vars = {"divider" : 1, "balance" : False})
+
+        print("Beginning backtest...")
+        # backtest does all the rest of the work
+        self.backtest(agent, test_x, test_y, len(test_data["Data"].keys()))
+
+    def test_backtest_with_more_features(self):
+        # As seen several times above, but without a train-test split
+        training_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                                 from_date = "2010-01-01", to_date = "2018-01-01")
+        training_data = format_into_percentages(training_data, formatting_basis = "daily open")
+        train_x, train_y = format_into_xy(training_data, num_features = 30, label_type = "bin",
+                label_type_vars = {"divider" : 1, "balance" : True})
+        train_ds = TickerDataset(train_x, train_y)
+        agent = Agent(train_ds)
+        agent._train(epochs = 500, verbose = True)
+        print("Training done!")
+
+        # The test data is simple to format, thanks to 'label_type = "float"'
+        test_data = get_data(path = self.path, tickers = ["AAPL", "GOOG", "AMZN"], 
+                             from_date = "2018-01-01", to_date = "2021-01-01")
+        test_data = format_into_percentages(test_data, formatting_basis = "daily open")
+        test_x, test_y = format_into_xy(test_data, num_features = 30, label_type = "float",
+                label_type_vars = {"divider" : 1, "balance" : False})
+
+        print("Beginning backtest...")
+        # backtest does all the rest of the work
+        self.backtest(agent, test_x, test_y, len(test_data["Data"].keys()))
